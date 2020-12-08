0edb19a5a229986820034e2bf6cbccf6e14d43d1
davidladeji <71533464+davidladeji@users.noreply.github.com>
2020-12-07 23:10:10 -0500

    Added files

---
+++ flaskblog/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '442c4a86a7b74b74c25570cdeb41e48f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
